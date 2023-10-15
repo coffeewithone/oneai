0922d59682735a3a5124815a21676add879a3d1c
coffeewithone <coffeeandone@gmail.com>
2023-10-15 02:15:13 -0400

    pnpm-fix

--- app/page.tsx
+++ app/page.tsx
@@ -158,15 +158,52 @@ export default function Home() {
                 const { children, className, node, ...rest } = props;
                 const match = /language-(\w+)/.exec(className || "");
                 const { ref, ...restWithoutRef } = rest;
+                const textAreaRef = useRef(null);
+                const [copyText, setCopyText] = useState("Copy");
+
+                const copyToClipboard = (e: any) => {
+                  const textToCopy = typeof children === "string" ? children : children?.toString() || "";
+                  navigator.clipboard.writeText(textToCopy);
+                  e.target.focus();
+                  setCopyText("Copied!"); // Change button text to "Copied!"
+
+                  setTimeout(() => {
+                    setCopyText("Copy"); // Change back to "Copy" after 3 seconds
+                  }, 3000);
+                };
+
                 return match ? (
-                  <SyntaxHighlighter
-                    {...restWithoutRef}
-                    showLineNumbers={true}
-                    children={String(children).replace(/\n$/, "")}
-                    style={dracula}
-                    language={match[1]}
-                    PreTag="div"
-                  />
+                  <div className="bg-[#282a36] rounded-md">
+                    <button
+                      type="button"
+                      className="m-1 rounded-md text-white bg-[#282a36] hover:bg-slate-700  focus:outline-none  font-medium  text-xs px-5 py-2.5 text-center inline-flex items-center mr-2"
+                      onClick={copyToClipboard}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="w-4 h-4 mr-1"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
+                        />
+                      </svg>
+                      {copyText}
+                    </button>
+                    <SyntaxHighlighter
+                      {...restWithoutRef}
+                      showLineNumbers={true}
+                      children={String(children).replace(/\n$/, "")}
+                      style={dracula}
+                      language={match[1]}
+                      PreTag="div"
+                    />{" "}
+                  </div>
                 ) : (
                   <code
                     {...rest}
